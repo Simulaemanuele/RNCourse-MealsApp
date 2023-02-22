84ad6ac53c50c6c290534e03649587361268ebe2
Emanuele Simula <simulaemanuele@gmail.com>
2023-02-22 11:59:26 +0100

    feat: added Favourities screen

---
+++ screens/FavouritesScreen.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const FavouritesScreen = () => {
+  return (
+    <View>
+      <Text>The favourites!</Text>
+    </View>
+  );
+};
+
+export default FavouritesScreen;
+
+const styles = StyleSheet.create({});
